2f11e9aeed98c0a902b13251e007a724bbe523cb
Jeroen Wolff <jeroen@studiowolff.nl>
2011-12-03 17:24:47 +0100

    Added 2 ruby examples found on the internet....

---
+++ examples/game_of_life2.rb
@@ -0,0 +1,75 @@
+class Grid
+  include Enumerable
+  class Cell
+    attr_reader :x, :y
+    [['born', 1], ['die', 0], ['unchanged', '@status']].each do |method,
+status|
+      module_eval(%{def #{method}; @grid[@x, @y] = #{status}; end})
+    end
+    def initialize(grid, x, y, status)
+      @grid = grid
+      @x, @y = x, y
+      @status = status
+    end
+    def alive?
+      @status == 1
+    end
+    def alive_neighbours
+      [[-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1,
+0]].inject(0) do |sum, cell|
+        sum += @grid[@x + cell[0], @y + cell[1]]
+      end
+    end
+    def to_s
+      alive? ? "o" : "."
+    end
+  end
+  def self.generate(width, height)
+    self.new(Array.new(height).collect { Array.new(width).collect {
+rand(2) } })
+  end
+  def initialize(seed)
+    @w, @h = seed.first.size, seed.size
+    @cells = seed
+  end
+  def [](x, y)
+    @cells[y % @h][x % @w]
+  end
+  def []=(x, y, value)
+    @nextgen[y % @h][x % @w] = value
+  end
+  def each(&blk)
+    @cells.each_with_index do |row, y|
+      row.each_with_index { |col, x| yield Cell.new(self, x, y, self[x,
+y]) }
+    end
+  end
+  def tick!
+    @nextgen = Array.new(@h).map { Array.new(@w).dup }
+    each do |cell|
+      if cell.alive?
+        (cell.alive_neighbours < 2 || cell.alive_neighbours > 3) ?
+cell.die : cell.unchanged
+      else
+        cell.alive_neighbours == 3 ? cell.born : cell.unchanged
+      end
+    end
+    @cells = @nextgen
+  end
+  def to_s
+    inject("") do |result, cell|
+      result << cell.to_s << (cell.x == (@w - 1) ? "\n" : "")
+    end
+  end
+end
+
+if $0 == __FILE__
+  grid = Grid.generate(ARGV[0] || 75, ARGV[1] || 20)
+  (ARGV[2] || 2000).times do |gen_id|
+    grid.tick!
+    puts "#" * (ARGV[0] || 75)
+    print grid
+    puts "#" * (ARGV[0] || 75)
+    puts "Generation n.#{gen_id + 1}"
+  end
+end
